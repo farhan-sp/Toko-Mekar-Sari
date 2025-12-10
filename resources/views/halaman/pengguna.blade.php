64bd12211f5111975b5ed3a6c3f0620c81b6284f
farhan-sp <farhansp03@gmail.com>
2025-12-10 07:02:35 +0700

    modified:   resources/views/halaman/pengguna.blade.php

--- resources/views/halaman/pengguna.blade.php
+++ resources/views/halaman/pengguna.blade.php
@@ -369,7 +369,6 @@
                                     name="username" 
                                     x-model="selectedPengguna.username"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 text-sm leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                                    required
                                 >
                             </div>
 
